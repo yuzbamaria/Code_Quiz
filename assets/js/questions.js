6f26e6d9fc37765c41ec649f03d1943fcf071502
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2023-12-09 15:02:11 +0000

    added a file questions.js and created fucntion displayQuestions

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+// Define the questions and the choices and the answers, put it in a variable in questions.js file
+
+const quizQuestions = [
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<js>", "<javascript>", "<scripting>", "<script>"],
+      answer: "<script>"
+    },
+
+    {
+      question: "Where is the correct place to insert a JavaScript?",
+      choices: [
+        "The <body> section", 
+        "Both the <head> section and the <body> section are correct", 
+        "The <head> section"
+        ],
+      answer: "Both the <head> section and the <body> section are correct"
+    },
+
+    {
+      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choices: ['<script href="xxx.js">', '<script name="xxx.js">', '<script src="xxx.js">'],
+      answer: '<script src="xxx.js">'
+    },
+    
+  ];
+  
