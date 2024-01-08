5146c2e1ff9bd3136c19944b7f43fb8bbc58b06e
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-01-08 22:54:30 +0000

    updated the link to get to highscores

--- logic.js
+++ logic.js
@@ -193,7 +193,7 @@ submitBtn.addEventListener('click', function(event) {
             localStorage.setItem('highscores', JSON.stringify(highScoresLocStor));
         }
     // Redirect the user to the 'highscores.html' page
-    window.location.pathname = '/highscores.html'
+    location.href = 'assets/js/highscores.html'
 });
 
 
